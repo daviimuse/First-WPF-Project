d8f372c34ff07355e331f47d9b4f165e4e8457b5
Davide <84710848+daviimuse@users.noreply.github.com>
2022-10-20 16:36:04 +0200

    Update usersLoginInfo.Table.sql

--- usersLoginInfo.Table.sql
+++ usersLoginInfo.Table.sql
@@ -1,8 +1,7 @@
 ﻿CREATE TABLE [dbo].[Table]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+    [Id] INT NOT NULL PRIMARY KEY, 
     [usrn] VARCHAR(50) NOT NULL, 
-    [mail] VARCHAR(50) NOT NULL, 
     [name] VARCHAR(50) NOT NULL, 
     [surname] VARCHAR(50) NOT NULL, 
     [psw] CHAR(32) NOT NULL
